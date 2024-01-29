171496178dff8e9110150232cb788c6d526b4db9
Shreeya Khurana <srkhuran@andrew.cmu.edu>
2024-01-29 13:47:22 -0500

    looping through all frames

--- scene_representation_script.py
+++ scene_representation_script.py
@@ -38,26 +38,6 @@ def get_instance_segm(frame_num, recorder_filename):
     instance_segm_image = cv2.imread(image_filename)
     return instance_segm_image
 
-# #instance_segmentation_output and rgb_output directories will be in a directory by the name of the recording file in carla/PythonAPI/examples
-
-# #Step 2: call get_RGB on specific frame number to obtain that particular image
-# rgb_img = get_RGB(frame_num, recording_file)
-# # cv2.imshow("RGB", rgb_img)
-# # cv2.waitKey(0) 
-# print("Got RGB Image")
-
-# #Step 3: call get_instance_segm on specific frame number to obtain that particular image
-# instance_segm_img = get_instance_segm(frame_num, recording_file)
-# # cv2.imshow("Instance Seg", instance_segm_img)
-# # cv2.waitKey(0) 
-# print("Got Instance Segmentation Image")
-# #Step 4: Run awareness_parser
-# #os.system("python awareness_parser.py -f " + recorder_parse_file)
-
-# awareness_data_file = "results/exp_nik-pilot-awdata.json"
-# awareness_df = pd.read_json(awareness_data_file, orient='index')
-# #print(awareness_df.keys())
-
 #create function from the parser to convert label into a T/F
 def get_label(user_input, aw_visible, aw_answer, type_bit=16):
     actors_num = len(aw_visible)
@@ -67,46 +47,6 @@ def get_label(user_input, aw_visible, aw_answer, type_bit=16):
             FoundCorrect = True
             break
     return FoundCorrect
-
-# #Step 5: get SA Label from awareness_frame
-# aw_visible = awareness_df["AwarenessData_Visible"][frame_num]
-# user_input = awareness_df["AwarenessData_UserInput"][frame_num]
-# aw_answer = awareness_df["AwarenessData_Answer"][frame_num]
-# if user_input == 0:
-#     sa_label = None
-# else:
-#     sa_label = get_label(user_input, aw_visible, aw_answer)
-# print(sa_label)
-
-
-# #Step 6: CAMERA CONVERSION
-# sensor_config = configparser.ConfigParser()
-# sensor_config.read('sensor_config.ini')
-
-# FOV = int(sensor_config['rgb']['fov'])
-# w = int(sensor_config['rgb']['width'])
-# h = int(sensor_config['rgb']['height'])
-# F = w / (2 * np.tan(FOV * np.pi / 360))
-
-# cam_info = {
-#     'F': F,
-#     'map_size' : 256,
-#     'pixels_per_world' : 5.5,
-#     'w' : w,
-#     'h' : h,
-#     'fy' : F,
-#     'fx' : 1.0 * F,
-#     'hack' : 0.4,
-#     'cam_height' : sensor_config['rgb']['z'],
-# }
-
-# K = np.array([
-# [cam_info['fx'], 0, cam_info['w']/2],
-# [0, cam_info['fy'], cam_info['h']/2],
-# [0, 0, 1]])
-
-# #Construct dictionary containing necessary data per frame for the focus hit points and vehicle location/orientation
-# recording_data_dict = get_data_dict(recorder_parse_file)
 
 #Following functions are from gaze attention map test.ipynb
 def ptsWorld2Cam(focus_hit_pt, world2camMatrix, K):
@@ -179,50 +119,15 @@ def world2pixels(focus_hit_pt, vehicle_transform, K, sensor_config):
     
     return pts_mid, pts_left, pts_right
 
-# focus_hit_pt = recording_data_dict[frame_num]["FocusInfo"]["HitPoint"]
-# loc = recording_data_dict[frame_num]["EgoVariables"]["VehicleLoc"]
-# rot = recording_data_dict[frame_num]["EgoVariables"]["VehicleRot"]
-# vehicle_loc = carla.Location(*(loc.squeeze()))/100
-# vehicle_rot = carla.Rotation(*(rot.squeeze()))
-# vehicle_transform = carla.Transform(location=vehicle_loc, rotation=vehicle_rot)
-
-# pts2d_mid, pts2d_left, pts2d_right = world2pixels(focus_hit_pt, vehicle_transform, K, sensor_config)
-# print(pts2d_mid)
-# print(rgb_img.shape)
-
-# #Plot the converted gaze coordinate onto the rgb image coordinate space
-# image = cv2.circle(rgb_img, pts2d_mid, radius=10, color=(255, 0, 255), thickness=-1)
-# cv2.imshow("RGB Current Frame Gaze", image)
-# cv2.waitKey(0) 
-
-# #Plot past 15 frames
-# heatmap_points = []
-# for i in range(1, 31):
-#     frame = frame_num - i
-#     focus_hit_pt_i = recording_data_dict[frame]["FocusInfo"]["HitPoint"]
-#     loc = recording_data_dict[frame]["EgoVariables"]["VehicleLoc"]
-#     rot = recording_data_dict[frame]["EgoVariables"]["VehicleRot"]
-#     vehicle_loc_i = carla.Location(*(loc.squeeze()))/100
-#     vehicle_rot_i = carla.Rotation(*(rot.squeeze()))
-#     vehicle_transform = carla.Transform(location=vehicle_loc_i, rotation=vehicle_rot_i)
-
-#     pts2d_mid, pts2d_left, pts2d_right = world2pixels(focus_hit_pt_i, vehicle_transform, K, sensor_config)
-#     heatmap_points.append(pts2d_mid)
-# print(heatmap_points)
-
-# for p in heatmap_points:
-#     cv2.circle(image, p, radius=3, color=(255, 0, 0), thickness=-1)
-# cv2.imshow("RGB Gaze Heatmap",image)
-# cv2.waitKey(0)
-
 #Step 1: Run replay_instance_segm.py to get the rgb images and the instance segmentation images
 recording_file = "/home/srkhuran-local/.config/Epic/CarlaUE4/Saved/exp_nik-pilot_12_05_2023_17_00_59.rec"
 recorder_parse_file = "%s/PythonAPI/examples/exp_nik-pilot.txt" % CARLA_PATH
 #os.system("python %s/PythonAPI/examples/replay_instance_segm.py -f %s -parse %s" %(CARLA_PATH, recording_file, recorder_parse_file))
 
-frame_num = 5890 #update this
+frame_num = 2 #update this
 
-def get_image_inputs(frame_num, recording_file, recorder_parse_file):
+def get_image_inputs(frame_num, recording_file, recorder_parse_file, recording_data_dict):
+    print("Frame Number: ", frame_num)
     #call get_RGB on specific frame number to obtain that particular image
     rgb_img = get_RGB(frame_num, recording_file) 
     print("Got RGB Image.")
@@ -233,7 +138,10 @@ def get_image_inputs(frame_num, recording_file, recorder_parse_file):
     
     #Run awareness_parser
     #os.system("python awareness_parser.py -f " + recorder_parse_file)
-    awareness_data_file = "results/exp_nik-pilot-awdata.json" #UPDATE THIS TO BE GENERAL    
+    fname = recorder_parse_file.split("/")[-1]
+    fname = fname.split(".txt")[0]
+    awareness_data_file_name = fname + "-awdata.json"
+    awareness_data_file = "results/%s" % awareness_data_file_name
     awareness_df = pd.read_json(awareness_data_file, orient='index')
     
     #get SA Label from awareness_frame
@@ -270,9 +178,6 @@ def get_image_inputs(frame_num, recording_file, recorder_parse_file):
     [cam_info['fx'], 0, cam_info['w']/2],
     [0, cam_info['fy'], cam_info['h']/2],
     [0, 0, 1]])
-
-    #Construct dictionary containing necessary data per frame for the focus hit points and vehicle location/orientation
-    recording_data_dict = get_data_dict(recorder_parse_file)
     
     focus_hit_pt = recording_data_dict[frame_num]["FocusInfo"]["HitPoint"]
     loc = recording_data_dict[frame_num]["EgoVariables"]["VehicleLoc"]
@@ -282,17 +187,21 @@ def get_image_inputs(frame_num, recording_file, recorder_parse_file):
     vehicle_transform = carla.Transform(location=vehicle_loc, rotation=vehicle_rot)
 
     pts2d_mid, pts2d_left, pts2d_right = world2pixels(focus_hit_pt, vehicle_transform, K, sensor_config)
-    print(pts2d_mid)
-    print(rgb_img.shape)
+    #print(pts2d_mid)
+    #print(rgb_img.shape)
 
     #Plot the converted gaze coordinate onto the rgb image coordinate space
     image = cv2.circle(rgb_img, pts2d_mid, radius=10, color=(255, 0, 255), thickness=-1)
-    cv2.imshow("RGB Current Frame Gaze", image)
-    cv2.waitKey(0) 
+    #cv2.imshow("RGB Current Frame Gaze", image)
+    #cv2.waitKey(0) 
 
     #Plot past 15 frames
     heatmap_points = []
-    for i in range(1, 31):
+    if frame_num <= 16:
+        end_point = frame_num
+    else:
+        end_point = 16
+    for i in range(1, end_point):
         frame = frame_num - i
         focus_hit_pt_i = recording_data_dict[frame]["FocusInfo"]["HitPoint"]
         loc = recording_data_dict[frame]["EgoVariables"]["VehicleLoc"]
@@ -303,16 +212,29 @@ def get_image_inputs(frame_num, recording_file, recorder_parse_file):
 
         pts2d_mid, pts2d_left, pts2d_right = world2pixels(focus_hit_pt_i, vehicle_transform, K, sensor_config)
         heatmap_points.append(pts2d_mid)
-    print(heatmap_points)
+    #print(heatmap_points)
 
     for p in heatmap_points:
         cv2.circle(image, p, radius=3, color=(255, 0, 0), thickness=-1)
-    cv2.imshow("RGB Gaze Heatmap",image)
-    cv2.waitKey(0)
+    #cv2.imshow("RGB Gaze Heatmap",image)
+    #cv2.waitKey(0)
+    if os.path.exists("gaze_heatmap/%s" % fname) is False:
+        os.makedirs("gaze_heatmap/%s" % fname)
+    output_file_name = "gaze_heatmap/%s/%s.jpg" % (fname, str(frame_num))
+    cv2.imwrite(output_file_name, image)
     
+
+def get_all_images(recorder_parse_file):
+    #Construct dictionary containing necessary data per frame for the focus hit points and vehicle location/orientation
+    recording_data_dict = get_data_dict(recorder_parse_file)
+    #print(recording_data_dict.keys())
+    for f in recording_data_dict.keys():
+        if f > 1:
+            get_image_inputs(f, recording_file, recorder_parse_file, recording_data_dict)
     
-get_image_inputs(frame_num, recording_file, recorder_parse_file)
-    
-    
+#recording_data_dict = get_data_dict(recorder_parse_file)
+#get_image_inputs(frame_num, recording_file, recorder_parse_file, recording_data_dict)
+get_all_images(recorder_parse_file)
+
     
     
