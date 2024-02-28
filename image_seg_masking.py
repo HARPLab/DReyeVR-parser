e2fb00eb45470017635384772c2ff7df67b8a517
Shreeya Khurana <srkhuran@andrew.cmu.edu>
2024-02-28 13:17:04 -0500

    functions to calculate id offset between rgb id calculation and dataframe id, and store that offset for a run

--- image_seg_masking.py
+++ image_seg_masking.py
@@ -2,10 +2,156 @@ import cv2
 import os
 import pandas as pd
 import numpy as np
+import carla
+import configparser
 from recorder_info_extracter import get_data_dict
 from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib import image
+from recorder_info_extracter import get_data_dict
+from scene_representation_script import ptsWorld2Cam, world2pixels
+
+def camera_to_2d(sensor_config, frame_num, obj_loc, recording_data_dict):
+    FOV = int(sensor_config['rgb']['fov'])
+    w = int(sensor_config['rgb']['width'])
+    h = int(sensor_config['rgb']['height'])
+    F = w / (2 * np.tan(FOV * np.pi / 360))
+    cam_info = {
+        'F': F,
+        'map_size' : 256,
+        'pixels_per_world' : 5.5,
+        'w' : w,
+        'h' : h,
+        'fy' : F,
+        'fx' : 1.0 * F,
+        'hack' : 0.4,
+        'cam_height' : sensor_config['rgb']['z'],
+    }
+    K = np.array([
+    [cam_info['fx'], 0, cam_info['w']/2],
+    [0, cam_info['fy'], cam_info['h']/2],
+    [0, 0, 1]])
+    obj_loc_scaled = np.array(obj_loc.squeeze())/100
+    loc = recording_data_dict[frame_num]["EgoVariables"]["VehicleLoc"]
+    rot = recording_data_dict[frame_num]["EgoVariables"]["VehicleRot"]
+    vehicle_loc = carla.Location(*(loc.squeeze()))/100
+    vehicle_rot = carla.Rotation(*(rot.squeeze()))
+    vehicle_transform = carla.Transform(location=vehicle_loc, rotation=vehicle_rot)
+
+    pts2d_mid, pts2d_left, pts2d_right = world2pixels(obj_loc_scaled, vehicle_transform, K, sensor_config)
+    return pts2d_mid, pts2d_left, pts2d_right
+
+
+def check_frame_quality(frame_num, recording_data_dict, sensor_config):
+    num_peds = 0
+    peds_locs = []
+    peds_dict = {}
+    visible_dict = recording_data_dict[frame_num]["AwarenessData"]["Visible"]
+    for id in visible_dict:
+        answer = int(visible_dict[id]["Answer"])
+        if answer <= 8 and answer >=1:
+            location_dict = visible_dict[id]["Location"]
+            obj_loc = np.asarray([location_dict["x"], location_dict["y"], location_dict["z"]])
+            obj_loc_2d = camera_to_2d(sensor_config, frame_num, obj_loc, recording_data_dict)[0]
+            if obj_loc_2d[0] > 0 and obj_loc_2d[1] > 0:
+                peds_locs.append(obj_loc_2d)
+                peds_dict[id] = visible_dict[id]
+                num_peds += 1
+    
+    if num_peds >= 2:
+        distances = []
+        on_edge = False
+        for i in range(len(peds_locs)):
+            p1 = peds_locs[i]
+            if p1[0] in range(0, 3) or p1[0] in range(797, 800) or p1[1] in range(0, 3) or p1[1] in range(597, 600):
+                on_edge = True
+            for j in range(i + 1, len(peds_locs)):
+                p2 = peds_locs[j]
+                distances.append(np.linalg.norm(np.array(p1) - np.array(p2)))
+        min_dist = min(distances)
+    
+        if min_dist >= 30 and (not on_edge):
+            return True, peds_dict
+    return False, peds_dict
+            
+def get_instseg_id_offset(recording_data_dict, images_dir, sensor_config, awareness_df):
+    f_num = 0
+    for frame_num in range(1, len(awareness_df)):
+        if os.path.exists("%s/instance_segmentation_output/%.6d.png" % (images_dir,  frame_num+2)):
+            good, peds_dict = check_frame_quality(frame_num+2, recording_data_dict, sensor_config)
+            if good:
+                f_num = frame_num
+                break
+    print(f_num)
+    # test, peds_dict = check_frame_quality(2380, recording_data_dict, sensor_config)
+    # f_num = 2380
+    # print(peds_dict)
+        
+    offset = -1
+
+    id_keys = sorted([int(k) for k in peds_dict])
+    inst_img_path = "%s/instance_segmentation_output/%.6d.png" % (images_dir,  f_num + 2)
+    inst_img  = Image.open(inst_img_path)
+    raw_img = np.array(inst_img)
+    plt.imshow(raw_img)
+    plt.show()
+    for i in range(len(id_keys)):
+        #get true id from dict keys 
+        true_id = str(id_keys[i])
+
+        #get location and convert from camera coordinates
+        location_dict = peds_dict[true_id]["Location"]
+        obj_loc = np.asarray([location_dict["x"], location_dict["y"], location_dict["z"]])
+        pts2d_mid = camera_to_2d(sensor_config, f_num+2, obj_loc, recording_data_dict)[0]
+
+        print(true_id)
+        print(pts2d_mid)
+        
+        try:
+            x, y =  pts2d_mid 
+            x = max(min(x, 800 - 1), 0)
+            y = max(min(y, 600 - 1), 0)
+            region = raw_img[max(y - 5, 0):min(y + 5, 600),
+                        max(x - 5, 0):min(x + 5, 800)]
+            region_b = region[:, :, 2]
+            region_g = region[:, :, 1]
+
+            b_values = np.unique(region_b)
+            g_values = np.unique(region_g)
+            
+            #b = raw_img[pts2d_mid[1], pts2d_mid[0], 2]
+            #g = raw_img[pts2d_mid[1], pts2d_mid[0], 1]
+            b = max(b_values)
+            g = max(g_values)
+            print(b)
+            print(g)
+        except:
+            continue
+        image = cv2.circle(raw_img, pts2d_mid, radius=10, color=(255, 0, 255), thickness=-1)
+        plt.imshow(image)
+        plt.show()
+            
+        #get rgb from that coordinate value in instance seg image
+        # b = raw_img[pts2d_mid[0], pts2d_mid[1], 2]
+        # g = raw_img[pts2d_mid[0], pts2d_mid[1], 1]
+
+        #calculate 256*b+g to get run id
+        if b != 0 and g != 0:
+            run_id = 256*b + g
+
+            #calculate offset
+            diff = int(true_id) - run_id
+            diff = abs(diff)
+
+            #offset should be the same across all objects (and across run)
+            if offset == -1:
+                offset = diff
+            else:
+                assert(offset == diff)
+    with open("%s/offset.txt" % images_dir, 'w') as file:
+        file.write(str(offset))
+
+    return offset
 
 
 def get_label_mask(id, instance_mask_file, aw_visible, aw_answer, user_input, type_bit=16):
@@ -62,32 +208,47 @@ def get_instance_seg_mask(inst_img_path, rgb_img_path, id):
     
     return mask
 
-def get_all_instance_segmentation_images(images_dir, awareness_df):
-    for frame_num in range(len(awareness_df)):
-        print(frame_num)
-        id_list = awareness_df["AwarenessData_Visible"][frame_num]
-        user_input = awareness_df["AwarenessData_UserInput"][frame_num]
-        aw_answer = awareness_df["AwarenessData_Answer"][frame_num]
-        inst_img = "%s/instance_segmentation_output/%.6d.png" % (images_dir, frame_num)
-        rgb_img = "%s/rgb_output/%.6d.png" % (images_dir, frame_num)
-        if not os.path.exists(inst_img) and not os.path.exists(rgb_img):
-            continue
+def get_all_instance_segmentation_images(rec_parse_file, images_dir, awareness_df, sensor_config_file):
+    recording_data_dict = get_data_dict(rec_parse_file)
+    sensor_config = configparser.ConfigParser()
+    sensor_config.read(sensor_config_file)
+    
+    if os.path.exists("%s/offset.txt" % images_dir):
+        with open("%s/offset.txt" % images_dir, 'r') as file:
+            offset = int(file.read())
+    else:
+        offset = get_instseg_id_offset(recording_data_dict, images_dir, sensor_config, awareness_df)
+    print(offset)
+
+    # for frame_num in range(1, len(awareness_df)):
+    #     print(frame_num)
+    #     id_list = awareness_df["AwarenessData_Visible"][frame_num]
+    #     user_input = awareness_df["AwarenessData_UserInput"][frame_num]
+    #     aw_answer = awareness_df["AwarenessData_Answer"][frame_num]
+    #     inst_img = "%s/instance_segmentation_output/%.6d.png" % (images_dir, frame_num)
+    #     rgb_img = "%s/rgb_output/%.6d.png" % (images_dir, frame_num)
+    #     if not os.path.exists(inst_img) and not os.path.exists(rgb_img):
+    #         continue
         
-        for id in id_list:
-            mask = get_instance_seg_mask(inst_img, rgb_img, id)
-            mask_img = Image.fromarray(mask)
-            mask_name = "%s/instance_masks/%.6d_%d.png" % (images_dir, frame_num, id)
-            mask_img.save(mask_name)
-            label_mask = get_label_mask(id, mask_name, id_list, aw_answer, user_input)
-            label_mask_img = Image.fromarray(label_mask)
-            label_name = "%s/label_masks/%.6d_%d.png" % (images_dir, frame_num, id)
-            label_mask_img.save(label_name)
+        
+        # for id in id_list:
+        #     mask = get_instance_seg_mask(inst_img, rgb_img, id)
+        #     mask_img = Image.fromarray(mask)
+        #     mask_name = "%s/instance_masks/%.6d_%d.png" % (images_dir, frame_num, id)
+        #     mask_img.save(mask_name)
+        #     label_mask = get_label_mask(id, mask_name, id_list, aw_answer, user_input)
+        #     label_mask_img = Image.fromarray(label_mask)
+        #     label_name = "%s/label_masks/%.6d_%d.png" % (images_dir, frame_num, id)
+        #     label_mask_img.save(label_name)
 
 
-awareness_parse_file = "/home/srkhuran-local/CarlaDReyeVR/DReyeVR-parser/results/exp_sud_21_SA-awdata.json"
+#awareness_parse_file = "/home/srkhuran-local/CarlaDReyeVR/DReyeVR-parser/results/exp_sud_21_SA-awdata.json"
+awareness_parse_file = "/home/srkhuran-local/CarlaDReyeVR/DReyeVR-parser/results/ines_51-awdata.json"
+rec_parse_file = "/home/srkhuran-local/CarlaDReyeVR/carla/PythonAPI/examples/exp_ines_51.txt"
 #awareness_parse_file = "/home/srkhuran-local/CarlaDReyeVR/DReyeVR-parser/results/exp_nik-pilot-awdata.json"
 awareness_df = pd.read_json(awareness_parse_file, orient='index')
 aw_visible = awareness_df["AwarenessData_Visible"]
-images_dir = "/home/srkhuran-local/CarlaDReyeVR/carla/PythonAPI/examples/exp_sud_21_02_02_2024_09_33_57/images"
+images_dir = "/home/srkhuran-local/CarlaDReyeVR/carla/PythonAPI/examples/exp_ines-51_02_13_2024_16_11_13/images"
+sensor_config_file = "/home/srkhuran-local/CarlaDReyeVR/carla/PythonAPI/examples/sensor_config.ini"
 #images_dir = "/home/srkhuran-local/CarlaDReyeVR/carla/PythonAPI/examples/exp_nik-pilot_12_05_2023_17_00_59/images"
-get_all_instance_segmentation_images(images_dir, awareness_df)
+get_all_instance_segmentation_images(rec_parse_file, images_dir, awareness_df, sensor_config_file)
--- recorder_info_extracter.py
+++ recorder_info_extracter.py
@@ -1,7 +1,6 @@
-import cv2
 import numpy as np
 
-#file_path = "/home/srkhuran-local/CarlaDReyeVR/carla/PythonAPI/examples/exp_nik-pilot.txt"
+file_path = "/home/srkhuran-local/CarlaDReyeVR/carla/PythonAPI/examples/exp_ines_51.txt"
 def get_data_dict(file_path):
    
 
@@ -19,25 +18,37 @@ def get_data_dict(file_path):
             splitstr = subStr.split(" ")
             frame_num = int(splitstr[0])
             frame_dict = {}
-        if "Create " and "vehicle" in line:
-            splitstr = line.split(" ")
-            vehicle_id = int(splitstr[2][:-1])
-            vehicleIds.append(vehicle_id)
-        if "Positions" in prev_line:
-            splitstr = prev_line.split(" ")
-            numPos = int(splitstr[2].strip())
-            positions = {}
-            for i in range(numPos):
-                nLine = lines[l+i]
-                nlinesplit = nLine.split(" ")
-                idNum = int(nlinesplit[3])
-                if idNum in vehicleIds:
-                    location_x = float(nlinesplit[5][1:-1])
-                    location_y = float(nlinesplit[6][:-1])
-                    location_z = float(nlinesplit[7][:-1])
-                    rotation = nlinesplit[9:]
-                    positions[idNum] = (location_x, location_y, location_z)
-            frame_dict["VehicleDict"] = positions
+        if "AwarenessData" in line:
+            string = line.split("AwarenessData:")[1]
+            parts = string.split(",")
+            visible_total = parts[0].split(":")[1]
+            visible_data_str = ",".join(parts[1:])
+            visible_data_str = visible_data_str.split("Visible:{")[1]
+            visible_data = visible_data_str.split("},")[:-1]
+            visible_objects = {}
+            for obj in visible_data:
+                vel_loc_dict = {}
+                id = 0
+                for item in obj.split(","):
+                    if item == "":
+                        continue
+                    key, value = item.split(":")
+                    if key == "{Id":
+                        id = value
+                    if key in ["Location", "Velocity"]:
+                        coords = value.split(" ")
+                        x = float(coords[0].split("=")[1])
+                        y = float(coords[1].split("=")[1])
+                        z = float(coords[2].split("=")[1])
+                        values = {"x":x, "y":y, "z":z}
+                        vel_loc_dict[key] = values
+                    if key == "Answer":
+                        vel_loc_dict[key] = value
+                visible_objects[id] = vel_loc_dict
+            aw_dict = {"VisibleTotal": visible_total,
+                    "Visible": visible_objects,
+                }
+            frame_dict["AwarenessData"] = aw_dict
         if "FocusInfo" in line:
             splitstr = line.split(",")
             focus_dict = {}
@@ -77,54 +88,9 @@ def get_data_dict(file_path):
     print("Recording Info Data Dictionary Created.")
     return recording_info_dict
 
-#info_dict = get_data_dict(file_path)
-#print(info_dict[1])
-#Map vehicle position and ID on each frame
-#for frame_num in vehicle_pos:
-# info_tuple = vehicle_pos[521]
-# id = info_tuple[0]
-# red = [0, 0, 255]
-# ##get frame image - frame_img
-# fileName = 'results/%.6d' % (521)
-# frame_img = cv2.imread(fileName)
-# coord_tuple = info_tuple[1:3]
-# frame_img[coord_tuple] = red
-# #cv2.imwrite('results/labeled/%.6d.png' % frame_num, frame_img)
-# cv2.imshow("image", frame_img)
-# cv2.waitKey(0) 
+# info_dict = get_data_dict(file_path)
+# print(info_dict[1879])
 
-"""
-def format_number(num):
-    res = str(num)
-    while len(res) < 6:
-        res = '0' + res
-    return res
-
-#Frome Awareness Image Parser run
-all_vehicleId_pixels = {}
-for frame_num in vehicle_pos:
-    vehicle_list = vehicle_pos[frame_num]
-    #get frame instance segmentation image
-    frame_str = format_number(frame_num + 1)
-    frame_img = cv2.imread("results/"+frame_str)
-
-    vehicleId_pixels_dict = {}
-    for vehicle in vehicle_list:
-        v_id = vehicle[0]
-        color_r = v_id/255.0
-        color_g = ((v_id & 0x00ff) >> 0) / 255.0
-        color_b = ((v_id & 0x00ff) >> 8) / 255.0
-        color_vector = [color_r, color_g, color_b]
-        pixels_list = []
-        #psuedocode!!
-        for i in range(frame_img.shape[0]):
-            for j in range(frame_img.shape[1]):
-                pixel = frame_img[i, j]
-                print(pixel)
-                if pixel == color_vector:
-                    pixels_list.append((i, j))
-        vehicleId_pixels_dict[v_id] = pixels_list
-""" 
 
 
 
